7984d047e80dcaf13cd36bfbe9ecbcf7c725d077
lumen-limitless <lumenlimitless@gmail.com>
2022-12-27 17:10:57 -0500

    message

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 A NextJS template with typescript and tailwindcss
+
+run npx sentry/wizard -i nextjs post install to activate sentry
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,21 +1,14 @@
 import { NextPage } from 'next'
 
-/* This example requires Tailwind CSS v2.0+ */
 const Page404: NextPage = () => {
   return (
     <>
-      {/*
-            This example requires updating your template:
-    
-            ```
-            <html class="h-full">
-            <body class="h-full">
-            ```
-          */}
       <div className="min-h-full bg-white px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
         <div className="mx-auto max-w-max">
           <main className="sm:flex">
-            <p className="text-4xl font-extrabold text-indigo-600 sm:text-5xl">404</p>
+            <p className="text-4xl font-extrabold text-indigo-600 sm:text-5xl">
+              404
+            </p>
             <div className="sm:ml-6">
               <div className="sm:border-l sm:border-gray-200 sm:pl-6">
                 <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl">
