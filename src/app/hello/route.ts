939b1b7174a4efe90b18837f7265262d47d6c131
lumen-limitless <lumenlimitless@gmail.com>
2023-05-06 13:22:17 -0400

    route log

--- src/app/hello/route.ts
+++ src/app/hello/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(request: NextRequest) {
+  console.log(request)
   return NextResponse.json({
     message: 'Hello, World!',
   })
