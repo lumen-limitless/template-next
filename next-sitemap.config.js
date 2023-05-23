b2f88ba3835434d2ed645d5161e7953ddd16f435
lumen-limitless <lumenlimitless@gmail.com>
2023-05-23 13:45:48 -0400

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_APP_URL || '',
   generateRobotsTxt: false, // (optional)
+  generateIndexSitemap: false,
   outDir: './src/app/',
   // ...other options
 }
