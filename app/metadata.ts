a38d3947b837945b8dfbaa78a8a5f10fde50fcec
lumen-limitless <lumenlimitless@gmail.com>
2023-09-14 21:41:48 -0400

    default metadata

--- app/metadata.ts
+++ app/metadata.ts
@@ -1,8 +1,8 @@
 import { Metadata } from 'next'
 
-const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || ''
+const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Next App'
 const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || ''
-const APP_URL = process.env.NEXT_PUBLIC_APP_URL || ''
+const APP_URL = process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'
 
 export const defaultMetadata: Metadata = {
   title: {
