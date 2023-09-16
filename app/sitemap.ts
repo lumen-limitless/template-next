74f6bbee87e800bdc589a7970fc5a6e11cd31330
lumen-limitless <lumenlimitless@gmail.com>
2023-09-16 00:38:19 -0400

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
+import { APP_URL } from '@/lib/constants'
 import { MetadataRoute } from 'next'
 
-const APP_URL = process.env.NEXT_PUBLIC_APP_URL || ''
 export default function Sitemap(): MetadataRoute.Sitemap {
   return [
     {
