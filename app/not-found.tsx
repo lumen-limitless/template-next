d779d3abe0ca7ee7d164a86756d2c348845868f9
lumen-limitless <lumenlimitless@gmail.com>
2023-10-06 19:48:09 -0400

    not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,4 @@
-type PageProps = {
-  params: {}
-  searchParams: Record<string, string | Array<string> | undefined>
-}
-
-export const metadata = {
-  title: 'Not Found',
-}
-export default async function Page({}: PageProps) {
+export default function NotFoundPage() {
   return (
     <>
       <h1>404 - page not found</h1>
