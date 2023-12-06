3aca4021721fc9eb605df2862e9c47380933d937
lumen-limitless <lumenlimitless@gmail.com>
2023-12-06 11:14:37 -0500

    Update Prettier configuration

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,14 @@
 /** @type {import('prettier').Config} */
 module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  plugins: [
+    'prettier-plugin-organize-imports',
+    'prettier-plugin-packagejson',
+    'prettier-plugin-tailwindcss',
+  ],
   tailwindFunctions: ['clsx', 'cn', 'cva'],
+  pluginSearchDirs: false,
 }
