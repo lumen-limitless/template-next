6da5305cedb4f4084b61f4d2e554c61b4ed8c39e
lumen-limitless <lumenlimitless@gmail.com>
2024-01-14 21:28:49 -0500

    Add hello route and update sharp version

---
+++ app/api/hello/route.ts
@@ -0,0 +1,8 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(req: NextRequest) {
+  console.log(req)
+  return NextResponse.json({
+    message: 'Hello, World!',
+  })
+}
