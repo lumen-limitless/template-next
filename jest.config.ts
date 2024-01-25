dcc4e7517e21b22b5227a9e67a9cb5cd4000c6a0
lumen-limitless <lumenlimitless@gmail.com>
2024-01-24 22:27:12 -0500

    Fix Jest configuration and add setup file

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,9 @@
-import nextJest from 'next/jest'
+import nextJest from 'next/jest';
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
-})
+});
 
 // Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
@@ -16,7 +16,9 @@ const config = {
     '^@/(.*)$': '<rootDir>/$1',
   },
   testEnvironment: 'jest-environment-jsdom',
-}
+
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+};
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(config)
+export default createJestConfig(config);
