73b0c882209dc9f13d82c9ad7aac20d7fbfc4835
lumen-limitless <lumenlimitless@gmail.com>
2024-11-23 17:05:28 -0500

    fix PageProps

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,28 @@
-interface PageProps {
-  params: {};
-  searchParams: Record<string, string | string[] | undefined>;
-}
+type PageProps = {
+  params: Promise<{}>;
+  searchParams: Promise<Record<string, string | string[] | undefined>>;
+};
 
-export default function Page({}: PageProps) {
+export const runtime = 'nodejs';
+
+export const revalidate = 0;
+
+export const dynamic = 'force-dynamic';
+
+export const metadata = {
+  title: '',
+};
+export default function Page({ params, searchParams }: PageProps) {
   return (
-    <div className="flex h-full w-full flex-grow flex-col items-center justify-center gap-12">
-      <object
-        data="/next.svg"
-        width={200}
-        height={48}
-        className="animate-pulse"
-      />
-    </div>
+    <>
+      <div className="flex h-full w-full flex-grow flex-col items-center justify-center gap-12">
+        <object
+          data="/next.svg"
+          width={200}
+          height={48}
+          className="animate-pulse"
+        />
+      </div>
+    </>
   );
 }
