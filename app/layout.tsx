f12f2b338fd56cc7300e2960479f8d672528a423
lumen-limitless <lumenlimitless@gmail.com>
2025-11-04 14:26:06 -0500

    format

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,8 +53,7 @@ export default function RootLayout(props: LayoutProps<"/">) {
 
         <footer id="footer" className="flex h-16 items-center border-t px-5">
           <p className="text-sm">
-            &copy; {new Date().getFullYear()} Lumen Limitless. All rights
-            reserved.
+            &copy; Lumen Limitless. All rights reserved.
           </p>
         </footer>
 
